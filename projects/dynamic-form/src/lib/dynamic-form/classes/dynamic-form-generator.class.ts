b0ccaa704d2fddcd732c7e27a8d88e33b0270e58
gorden <limguancheng@hotmail.com>
2022-04-19 15:24:45 +0800

    remove temp additional control

--- projects/dynamic-form/src/lib/dynamic-form/classes/dynamic-form-generator.class.ts
+++ projects/dynamic-form/src/lib/dynamic-form/classes/dynamic-form-generator.class.ts
@@ -331,17 +331,6 @@ export class DynamicFormGenerator {
                   element.async_validation_fn
                 )
               );
-              if (elementControl.type === 'datalist') {
-                group.addControl(
-                  `${elementControl.name}_key`,
-                  this.createControl2(
-                    false,
-                    undefined,
-                    undefined,
-                    undefined
-                  )
-                );
-              }
             });
             group.setValidators(element.validation_fn ?? null);
             break;
